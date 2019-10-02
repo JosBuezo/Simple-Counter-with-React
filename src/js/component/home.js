dcc48ddd603bbc6708155da0ea1331e2e249c242
JosBuezo <joselynbuezo@gmail.com>
2019-10-02 19:08:14 +0000

    submit simple counter

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,21 +5,58 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	state = {
+		timerOn: false,
+		timerStart: 0,
+		timerTime: 0
+	};
+
+	startTimer = () => {
+		this.setState({
+			timerOn: true,
+			timerTime: this.state.timerTime,
+			timerStart: Date.now() - this.state.timerTime
+		});
+		this.timer = setInterval(() => {
+			this.setState({
+				timerTime: Date.now() - this.state.timerStart
+			});
+		}, 10);
+	};
+
+	stopTimer = () => {
+		this.setState({ timerOn: false });
+		clearInterval(this.timer);
+	};
+
+	resetTimer = () => {
+		this.setState({
+			timerStart: 0,
+			timerTime: 0
+		});
+	};
+
 	render() {
+		const { timerTime } = this.state;
+		let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+		let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+		let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+		let hours = ("0" + Math.floor(timerTime / 3600000)).slice(-2);
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div className="Stopwatch">
+				<div className="Stopwatch-display">
+					<i class="fas fa-stopwatch"> </i>
+					{hours} : {minutes} : {seconds} : {centiseconds}
+				</div>
+				{this.state.timerOn === false &&
+					this.state.timerTime === 0 && (
+						<button
+							type="button"
+							class="btn btn-danger"
+							onClick={this.startTimer}>
+							Start
+						</button>
+					)}
 			</div>
 		);
 	}
